81adc3932e0682007cb78f59ef66c2ea491fa0e4
yurii <yuriydorosh2005@gmail.com>
2023-01-19 20:28:34 +0200

    refactoring

--- remember_me.py
+++ remember_me.py
@@ -1,20 +1,27 @@
 import json
 
 
-# Load the username if it has already been loaded
-# Otherwise, ask for the username and save it
-
-def greet_user():
+def get_stored_username():
+    # Get a name
     filename = 'username.json'
     try:
         with open(filename) as f:
             username = json.load(f)
     except FileNotFoundError:
+        return None
+
+
+def greet_user():
+    # Greet user
+    username = get_stored_username()
+    if username:
+        print(f'Welcome back, {username}!')
+    else:
         username = input('What is your name? ')
+        filename = 'username.json'
         with open(filename, 'w') as f:
             json.dump(username, f)
             print(f'We`ll remember you when you come back, {username}!')
-    else:
-        print(f'Welcome back, {username}!')
+
 
 greet_user()
